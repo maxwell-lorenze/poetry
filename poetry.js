c27ff64c94a004686df5dca60824020f93a87fce
maxwell-lorenze <33530090+maxwell-lorenze@users.noreply.github.com>
2020-05-01 20:26:17 -0700

    Add files via upload

---
+++ poetry.js
@@ -0,0 +1,26 @@
+let bouncing_balls = []
+let floating_rectangles = []
+if(frameCount[]< 500){
+function setup() {
+let detailX;
+    // create a canvas that fills the browser window and attach it to your HTML
+    createCanvas(windowWidth, windowHeight).parent('p5')
+    background(245)
+
+    // the p5 variables "width" and "height" hold the dimensions of the canvas
+
+
+}
+
+function draw() {
+}
+
+
+
+// if the window is resized, resize the canvas to match
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight)
+}}
+else{
+  window.location.href = 'https://www.google.com'
+}
